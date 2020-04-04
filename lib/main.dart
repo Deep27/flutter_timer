ba6b78cf5808887a05ee6f617beb0431a377584d
Deep27 <geep2795@gmail.com>
2020-04-04 19:05:03 +0300

    Added TimerWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timer/bloc/timer/timer_bloc.dart';
 import 'package:flutter_timer/ticker.dart';
+import 'package:flutter_timer/widgets/timer_widget.dart';
 
 void main() => runApp(FlutterTimer());
 
@@ -11,34 +12,14 @@ class FlutterTimer extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Timer',
       theme: ThemeData(
-          primaryColor: Color.fromRGBO(109, 234, 255, 1),
-          accentColor: Color.fromRGBO(72, 74, 126, 1),
-          brightness: Brightness.dark),
+        primaryColor: Color.fromRGBO(109, 234, 255, 1),
+        accentColor: Color.fromRGBO(72, 74, 126, 1),
+        brightness: Brightness.dark,
+      ),
       home: BlocProvider(
         create: (context) => TimerBloc(ticker: Ticker()),
-        child: Timer(), // @TODO
+        child: TimerWidget(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(),
     );
   }
 }
---
+++ lib/widgets/timer_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/bloc/timer/timer_bloc.dart';
+
+class TimerWidget extends StatelessWidget {
+  static const TextStyle timerTextStyle =
+      TextStyle(fontSize: 60, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100.0),
+            child: Center(
+              child: BlocBuilder<TimerBloc, TimerState>(
+                builder: (context, state) {
+                  final String minutesStr = ((state.duration / 60) % 60) // @TODO %60?
+                      .floor()
+                      .toString()
+                      .padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
+                  return Text(
+                    '$minutesStr:$secondsStr',
+                    style: Timer.timerTextStyle,
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
