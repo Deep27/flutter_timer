998df3b1be2f2f73da2fa53f37fa1cb9766df5a3
Deep27 <geep2795@gmail.com>
2020-04-04 19:38:23 +0300

    Version 1.0

--- lib/widgets/timer_widget.dart
+++ lib/widgets/timer_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timer/bloc/timer/timer_bloc.dart';
+import 'package:flutter_timer/widgets/actions_widget.dart';
+import 'package:flutter_timer/widgets/waves_background_widget.dart';
 
 class TimerWidget extends StatelessWidget {
   static const TextStyle timerTextStyle =
@@ -10,29 +12,42 @@ class TimerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Timer')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 100.0),
-            child: Center(
-              child: BlocBuilder<TimerBloc, TimerState>(
-                builder: (context, state) {
-                  final String minutesStr = ((state.duration / 60) % 60) // @TODO %60?
-                      .floor()
-                      .toString()
-                      .padLeft(2, '0');
-                  final String secondsStr =
-                      (state.duration % 60).floor().toString().padLeft(2, '0');
-                  return Text(
-                    '$minutesStr:$secondsStr',
-                    style: TimerWidget.timerTextStyle,
-                  );
-                },
+          WavesBackgroundWidget(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(
+                  child: BlocBuilder<TimerBloc, TimerState>(
+                    builder: (context, state) {
+                      final String minutesStr =
+                          ((state.duration / 60) % 60) // @TODO %60?
+                              .floor()
+                              .toString()
+                              .padLeft(2, '0');
+                      final String secondsStr = (state.duration % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      return Text(
+                        '$minutesStr:$secondsStr',
+                        style: TimerWidget.timerTextStyle,
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-          )
+              BlocBuilder<TimerBloc, TimerState>(
+                condition: (previousState, state) =>
+                    state.runtimeType != previousState.runtimeType,
+                builder: (context, state) => ActionsWidget(),
+              ),
+            ],
+          ),
         ],
       ),
     );
